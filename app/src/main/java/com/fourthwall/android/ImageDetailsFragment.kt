31dbd40cf0e4856156de4060fb7997dffde4cdeb
sancarbar <sancarbar@gmail.com>
2021-11-06 09:29:25 -0500

    1. Initial commit using Base Activity template modified with destinations needed, theme and app icon.

---
+++ app/src/main/java/com/fourthwall/android/ImageDetailsFragment.kt
@@ -0,0 +1,43 @@
+package com.fourthwall.android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.fourthwall.android.databinding.FragmentImagesDetailsBinding
+
+/**
+ * A ImageDetails [Fragment] as the second destination in the navigation.
+ */
+class ImageDetailsFragment : Fragment() {
+
+    private var _binding: FragmentImagesDetailsBinding? = null
+
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentImagesDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_ImageDetailsFragment_to_ImagesGalleryFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/fourthwall/android/ImagesGalleryFragment.kt
@@ -0,0 +1,42 @@
+package com.fourthwall.android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.fourthwall.android.databinding.FragmentImagesGalleryBinding
+
+/**
+ * The Images Gallery [Fragment] as the default destination in the navigation.
+ */
+class ImagesGalleryFragment : Fragment() {
+
+    private var _binding: FragmentImagesGalleryBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentImagesGalleryBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_ImagesGalleryFragment_to_ImageDetailsFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
